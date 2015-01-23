1083df1521e4b751bb3dc8808dc57db68393dca8
wangzaicheng <wcatp19891104@gmail.com>
2015-01-22 22:47:48 -0800

    valid par

---
+++ Valid Parentheses.py
@@ -0,0 +1,19 @@
+__author__ = 'wangzaicheng'
+class Solution:
+    # @return a boolean
+    def isValid(self, s):
+        if len(s) % 2 == 1:
+            return False
+        st = list()
+        for char in s:
+            if char in {'(', '{', '['}:
+                st.append(char)
+            else:
+                if len(st) == 0:
+                    return False
+                mark = st.pop()
+                if (mark, char) not in {('(', ')'), ('{','}'), ('[', ']')}:
+                    return False
+        if len(st) != 0:
+            return False
+        return True
