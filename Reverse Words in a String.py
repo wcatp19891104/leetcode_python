2f248ff7a8c21c5250ac995406aeaeffb8c761b7
wangzaicheng <wcatp19891104@gmail.com>
2015-01-25 20:39:25 -0800

    fix

--- Reverse Words in a String.py
+++ Reverse Words in a String.py
@@ -5,7 +5,8 @@ class Solution:
     # @param s, a string
     # @return a string
     def reverseWords(self, s):
-        return ' '.join({s[i].strip() for i in range(len(s) - 1, -1, -1)})
+        s = s.split(' ')
+        return ' '.join([s[i] for i in range(len(s) - 1, -1, -1) if s[i] != ''])
 
 if __name__ == "__main__":
     s = Solution()
